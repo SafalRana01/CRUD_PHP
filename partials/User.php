6301b10dd1c60bd09ae2c062b84fc5e54fc578c2
Safal Rana <magarsafal61@gmail.com>
2024-08-30 21:55:10 +0545

    function to upload photo is done

--- partials/User.php
+++ partials/User.php
@@ -86,8 +86,32 @@
             return $result;
 
         }
+
+
         // function to upload photo
 
+        public function uploadPhoto($file){
+            if(!empty($file)){
+                $fileTempPath=$file['tmp_name'];
+                $fileName=$file['name'];
+                $fileType=$file['type'];
+                $fileNameCmps=explode('.',$fileName);
+                $fileExtension=strtolower(end($fileNameCmps));
+                $newFileName=md5(time().$fileName) . '.'.
+                $fileExtension;
+                $allowedExtn=["png","jpg","jpeg"];
+
+                if(in_array($fileExtension,$allowedExtn)){
+                    $uploadFileDir=getcwd().'/uploads/';
+                    $destFiLePath=$uploadFileDir . $newFileName;
+
+                    if(move_upload_file($fileTempPath, $destFiLePath)){
+                        return $newFileName;
+                    }
+                }
+            }
+
+        }
         // function to delete
 
 
